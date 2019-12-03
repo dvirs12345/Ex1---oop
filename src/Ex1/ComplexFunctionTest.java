eca4f2c1609e298fd30d0d70eb32e236fb6ac049
Eli Ruvinov <eli4056@gmail.com>
2019-12-04 00:33:37 +0200

    add ComplexFunctionTest

---
+++ src/Ex1/ComplexFunctionTest.java
@@ -0,0 +1,73 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import com.sun.org.apache.xpath.internal.operations.Operation;
+
+class ComplexFunctionTest {
+	
+	@Test
+	public void testComplexFunction() {
+		// TODO Auto-generated method stub
+	}
+	
+	@Test
+	public void testF() {
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testInitFromString() {
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testCopy() {
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testPlus() { 
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testMul() {
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testDiv() { 
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testMax() { 
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testMin() { 
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testComp() { 
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testLeft() {	
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testRight() {
+		// TODO Auto-generated method stub
+	}
+
+	@Test
+	public void testGetOp() {
+		// TODO Auto-generated method stub
+	}
+}
