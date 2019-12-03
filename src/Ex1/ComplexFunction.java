00a940ccc214264aec61322d519c22456f584699
Eli Ruvinov <eli4056@gmail.com>
2019-12-03 19:04:50 +0200

    adding the simple functions

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -1,7 +1,18 @@
 package Ex1;
 
-public class ComplexFunction implements complex_function{
+import com.sun.org.apache.xpath.internal.operations.Operation;
 
+public class ComplexFunction implements complex_function{
+	ComplexFunction left; 
+	ComplexFunction right;
+	Operation op;
+	
+	public ComplexFunction(ComplexFunction f) {
+		this.left = f.left;
+		this.right = f.right;
+		this.op = f.op;
+	}
+	
 	@Override
 	public double f(double x) {
 		// TODO Auto-generated method stub
@@ -16,62 +27,76 @@ public class ComplexFunction implements complex_function{
 
 	@Override
 	public function copy() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ComplexFunction(this);
 	}
 
 	@Override
 	public void plus(function f1) {
-		// TODO Auto-generated method stub
-		
+		ComplexFunction temp = new ComplexFunction();
+		temp.left = new ComplexFunction(this);
+		temp.right = f1;
+		temp.op = Operation.plus;
+		this = temp;
 	}
 
 	@Override
 	public void mul(function f1) {
-		// TODO Auto-generated method stub
-		
+		ComplexFunction temp = new ComplexFunction();
+		temp.left = new ComplexFunction(this);
+		temp.right = f1;
+		temp.op = Operation.Times;
+		this = temp;
 	}
 
 	@Override
 	public void div(function f1) {
-		// TODO Auto-generated method stub
-		
+		ComplexFunction temp = new ComplexFunction();
+		temp.left = new ComplexFunction(this);
+		temp.right = f1;
+		temp.op = Operation.Divid;
+		this = temp;
 	}
 
 	@Override
 	public void max(function f1) {
-		// TODO Auto-generated method stub
-		
+		ComplexFunction temp = new ComplexFunction();
+		temp.left = new ComplexFunction(this);
+		temp.right = f1;
+		temp.op = Operation.Max;
+		this = temp;
 	}
 
 	@Override
 	public void min(function f1) {
-		// TODO Auto-generated method stub
-		
+		ComplexFunction temp = new ComplexFunction();
+		temp.left = new ComplexFunction(this);
+		temp.right = f1;
+		temp.op = Operation.Min;
+		this = temp;
 	}
 
 	@Override
 	public void comp(function f1) {
-		// TODO Auto-generated method stub
-		
+		ComplexFunction temp = new ComplexFunction();
+		temp.left = new ComplexFunction(this);
+		temp.right = f1;
+		temp.op = Operation.Comp;
+		this = temp;
 	}
 
 	@Override
-	public function left() {
-		// TODO Auto-generated method stub
-		return null;
+	public function left() {		
+		return this.left;
 	}
 
 	@Override
 	public function right() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.right;
 	}
 
 	@Override
 	public Operation getOp() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.op;
 	}
 
 }
