30b8f05cdc66713cafc8932b4cb48b37e0770525
Eli Ruvinov <eli4056@gmail.com>
2019-12-09 11:18:41 +0200

    Functions_GUITest

---
+++ src/Ex1/Functions_GUITest.java
@@ -0,0 +1,48 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Functions_GUITest {
+
+	@Test
+	void testInitFromFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSaveToFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testDrawFunctionsIntIntRangeRangeInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testDrawFunctionsString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+}
