eb8bab801a66d352121fc915112b18a44baf0229
Eli Ruvinov <eli4056@gmail.com>
2019-12-10 23:49:45 +0200

    working drawFunction in GUI

--- OPP Ex1/src/myMath/Functions_GUI.java
+++ OPP Ex1/src/myMath/Functions_GUI.java
@@ -1,9 +1,11 @@
 package myMath;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
+import com.google.gson.*;
 
 public class Functions_GUI implements functions {
 	private LinkedList<function> arr;
@@ -88,14 +90,63 @@ public class Functions_GUI implements functions {
 		Iterator<function> it = this.arr.iterator();
 		while (it.hasNext()) {
 			function function = it.next();
-			file += function.toString();
+			file += function.toString()+"\n";
 		}
 	}
 
 	@Override
 	public void drawFunctions(int width, int height, Range rx, Range ry, int resolution) {
 		// TODO Auto-generated method stub
+		StdDraw.setCanvasSize(width, height);
+		StdDraw.setXscale(rx.get_min(),rx.get_max());
+		StdDraw.setYscale(ry.get_min(),ry.get_max());
+		StdDraw.setPenRadius(0.0001);
+		StdDraw.setFont();
+		for (int i = (int) rx.get_min(); i < rx.get_max(); i+=((rx.get_max()-rx.get_min())/20)) {
+			StdDraw.line(i, ry.get_min(), i, ry.get_max());
+		}
+		for (int i = (int) ry.get_min(); i < ry.get_max(); i+=((rx.get_max()-rx.get_min())/20)) {
+			StdDraw.line(-width/2, i, width/2, i);
+		}
+		StdDraw.setPenRadius(0.002);
+		Iterator<function> it = this.arr.iterator();
+		Color[] colors = { 
+				Color.BLUE,
+				Color.CYAN,
+				Color.GREEN,
+				Color.ORANGE,
+				Color.MAGENTA,
+				Color.PINK,
+				Color.RED,
+				Color.YELLOW
+		};
+		int j = 0;
+		while (it.hasNext()) {
+			StdDraw.setPenColor(colors[j%colors.length]);
+			function f = it.next();
+			double[] x = new double[width*resolution];
+			double[] y = new double[width*resolution];
+			for (int i = 0; i < width*resolution ; i++) {
+				x[i] = rx.get_min() +i*(rx.get_max()-rx.get_min())/(width*resolution);
+				y[i] = f.f(x[i]);
+			}
+			StdDraw.polygon(x, y);
+			j++;
+		}
 		
+	}
+	
+	public static void main(String[] args){
+		Functions_GUI fg = new Functions_GUI();
+		fg.add( new Polynom("X^2"));
+		fg.add( new Polynom("X+3"));
+		fg.add( new Polynom("3"));
+		fg.add( new Polynom("-x+1"));
+		fg.add( new Polynom("X"));
+		fg.add( new Polynom("-0.5x"));
+		fg.add( new Polynom("X^3"));
+		fg.add( new Sinus(new Polynom("5X+1")));
+		fg.drawFunctions(1200, 600, new Range(-9, 11), new Range(-3, 7), 300);
 	}
 
 	@Override
